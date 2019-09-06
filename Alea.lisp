54d8ff7add8a8254f085b6dfdadc0a293fdcd281
j-bresson <bresson@ircam.fr>
2019-09-06 16:50:30 +0200

    om7 compatibility => v 1.2

--- Alea.lisp
+++ Alea.lisp
@@ -1,55 +1,43 @@
-;--------------------------------------------------
-;Package Definition (Optional, else use package :OM) 
-;--------------------------------------------------
+;============================================================================
+; Alea
+; Aleatoric-functions library for OM
+;============================================================================
+;
+;   This program is free software. For information on usage 
+;   and redistribution, see the "LICENSE" file in this distribution.
+;
+;   This program is distributed in the hope that it will be useful,
+;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+;
+;============================================================================
 
-(defpackage "ALEA" 
-  (:use "COMMON-LISP" "OpenMusic"))
+;;; LIB LOADER FOR OM6
+
+(in-package :om)
+
+(mapc #'om::compile&load 
+      (list 
+       (om::om-relative-path '("sources") "package")
+       (om::om-relative-path '("sources") "distributions")
+       (om::om-relative-path '("sources") "random-walks")
+       (om::om-relative-path '("sources") "alea-sequences")
+       (om::om-relative-path '("sources") "outils")
+       ))
 
 
+(om::fill-library 
+ '(("distributions" nil nil (ran ran01 choix choixmultiple distexp  expobi distlin
+                                 distcauchy distlog distCsHp distarsin poisson triang weilbull
+                                 gauss gamma beta bfp-to-distribution) nil)
+   ("alea-seq" nil nil (not-centr alea-seq linea-seq triang-seq) nil)
+   ("random-walks" nil nil (brownian1 brownian2  
+                                      randwalk1  randwalk2 randwalkX 
+                                      achorripsis i1/f seq1/f 
+                                      markov1 markov2 Ana-Mark Ana-Mark1 Ana-Mark2 
+                                      transition2) nil)
+   ("tools" nil nil ( zoom1 zoom2  zoom3 zoom4 filtre1  filtre2  filtre3 filtre4) nil)))
 
-(in-package "ALEA")
-
-;;; mettre dans un des fichiers sources...
-(defun om-random-value (val)
-   (om::om-random-value val))
-
-;--------------------------------------------------
-;Variable definiton with files to load 
-;--------------------------------------------------
-
-(defvar *OMAlea-lib-files* nil)
-(setf *OMAlea-lib-files* (list (om::om-relative-path '("sources") "distributions")
-                               (om::om-relative-path '("sources") "random-walks")
-                               (om::om-relative-path '("sources") "alea-sequences")
-                               (om::om-relative-path '("sources") "outils")
-                               ))
-
-;--------------------------------------------------
-;Loading files 
-;--------------------------------------------------
-
-(mapc #'om::compile&load *OMAlea-lib-files*)
-
-;--------------------------------------------------
-; OM subpackages initialization
-; ("sub-pack-name" subpacke-lists class-list function-list class-alias-list)
-;--------------------------------------------------
-(defvar *subpackages-list* nil)
-(setf *subpackages-list*
-      '(("distributions" nil nil (ran ran01 choix choixmultiple distexp  expobi distlin
-                                      distcauchy distlog distCsHp distarsin poisson triang weilbull
-                                      gauss gamma beta bfp-to-distribution) nil)
-        ("alea-seq" nil nil (not-centr alea-seq   linea-seq triang-seq) nil)
-        ("random-walks" nil nil ( brownian1 brownian2  randwalk1  randwalk2  
-   randwalkX achorripsis i1/f seq1/f markov1 markov2 Ana-Mark Ana-Mark1 Ana-Mark2 transition2) nil)
-        ("tools" nil nil ( zoom1 zoom2  zoom3 zoom4 filtre1  filtre2  filtre3 filtre4) nil)))
-
-;--------------------------------------------------
-;filling packages
-;--------------------------------------------------
-
-(om::fill-library *subpackages-list*)
 (om::add-lib-alias "OMAlea" "Alea")
 
-(om::set-lib-release 1.1)
-
+(om::set-lib-release 1.2)
---
+++ sources/package.lisp
@@ -0,0 +1,22 @@
+;============================================================================
+; Alea
+; Aleatoric-functions library for OM
+;============================================================================
+;
+;   This program is free software. For information on usage 
+;   and redistribution, see the "LICENSE" file in this distribution.
+;
+;   This program is distributed in the hope that it will be useful,
+;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+;
+;============================================================================
+
+(defpackage "ALEA" 
+  (:use "COMMON-LISP" "OpenMusic"))
+
+(in-package "ALEA")
+
+;;; mettre dans un des fichiers sources...
+(defun om-random-value (val)
+  (om::om-random-value val))
